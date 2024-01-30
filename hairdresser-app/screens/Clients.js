774099344791b85437503f4736a0fee64ae6f13b
Vasco Raminhos <vascoraminhos@hotmail.com>
2024-01-30 20:33:47 +0200

    +Added search to client list

--- hairdresser-app/screens/Clients.js
+++ hairdresser-app/screens/Clients.js
@@ -1,12 +1,44 @@
-import {SafeAreaView, StyleSheet, View} from "react-native";
+import {SafeAreaView, StyleSheet, TextInput, TouchableOpacity, View} from "react-native";
 import commonStyles from "../styles/commonStyles";
 import PlusCircle from "../components/PlusCircle/PlusCircle";
 import useClients from "../hooks/useClients";
 import ClientList from "../components/ClientList/ClientList";
+import {useEffect, useState} from "react";
+import Ionicons from "@expo/vector-icons/Ionicons";
 
 export default function Clients({navigation}) {
 
 	const [clients, , fetchClients] = useClients();
+	const [searchText, setSearchText] = useState("");
+
+	function getFilteredClients() {
+		let filteredClients = undefined;
+		if (searchText.trim().length > 0) {
+			const predicate = (client) => {
+				if (client.name.toLowerCase().includes(searchText.toLowerCase())) {
+					return true;
+				} else return client.phone.includes(searchText);
+			}
+
+			return clients.filter(predicate);
+		} else {
+			return clients;
+		}
+
+	}
+
+	useEffect(() => {
+		navigation.setOptions({
+			headerRight: () => (
+				<View style={styles.searchContainer}>
+					<TextInput onChangeText={text => setSearchText(text)} style={styles.searchInput}/>
+					<View style={styles.searchButton} onPress={() => setIsModalVisible(true)}>
+						<Ionicons name="search" size={20} color={"black"}/>
+					</View>
+				</View>
+			)
+		});
+	}, []);
 
 	function navigateToEdit(data) {
 		data.birthday = data.birthday.toISOString();
@@ -15,7 +47,7 @@ export default function Clients({navigation}) {
 
 	return (
 		<SafeAreaView style={[styles.container, commonStyles.relativeContainer, commonStyles.flex1Container]}>
-			<ClientList clients={clients} fetchClients={fetchClients} onItemPress={navigateToEdit}/>
+			<ClientList clients={getFilteredClients()} fetchClients={fetchClients} onItemPress={navigateToEdit}/>
 			<View style={styles.plusContainer}>
 				<PlusCircle/>
 			</View>
@@ -28,9 +60,32 @@ const styles = StyleSheet.create({
 		backgroundColor: "white"
 	},
 
+	searchContainer: {
+		borderWidth: 1,
+		borderColor: "gray",
+		borderRadius: 4,
+		display: "flex",
+		flexDirection: "row",
+		marginRight: 15,
+		justifyContent: "center",
+		alignItems: "center",
+	},
+
+	searchButton: {
+		paddingRight: 5
+	},
+
+	searchInput: {
+		fontSize: 16,
+		padding: 3,
+		paddingHorizontal: 8,
+		marginBottom: 0,
+		width: 100
+	},
+
 	plusContainer: {
 		position: "absolute",
 		bottom: 10,
 		right: 10
-	}
+	},
 });
--- hairdresser-app/screens/navigators/HomeDrawer.js
+++ hairdresser-app/screens/navigators/HomeDrawer.js
@@ -1,14 +1,18 @@
-import { createDrawerNavigator } from '@react-navigation/drawer';
+import {createDrawerNavigator} from '@react-navigation/drawer';
 import Home from "../Home";
 import Clients from "../Clients";
+import {StyleSheet} from "react-native";
+import {useState} from "react";
 
 const Drawer = createDrawerNavigator();
 
 export default function HomeDrawer() {
+	const [clientSearch, setClientSearch] = useState("");
+
 	return (
 		<Drawer.Navigator screenOptions={{swipeEnabled: true, swipeEdgeWidth: 100}}>
-		 	<Drawer.Screen name="Home" component={Home} options={{headerShown: false}}/>
-			<Drawer.Screen name="Clients" component={Clients} />
+			<Drawer.Screen name="Home" component={Home} options={{headerShown: false}}/>
+			<Drawer.Screen name="Clients" component={Clients}/>
 		</Drawer.Navigator>
 	);
 }
