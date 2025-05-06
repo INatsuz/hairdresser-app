3ef91aebe89f64d6e997772f7369c1becffaed27
Vasco <vascoraminhos@hotmail.com>
2025-05-07 02:33:03 +0300

    Fixed wildcard for admin panel path matching for express 5

--- hairdresser-backend/routes/admin.js
+++ hairdresser-backend/routes/admin.js
@@ -3,7 +3,7 @@ const path = require("path");
 const router = express.Router();
 
 /* GET home page. */
-router.get('*', function (req, res, next) {
+router.get('/{*admin}', function (req, res, next) {
 	res.sendFile(path.join(__dirname, "..", "public", "index.html"));
 });
 
